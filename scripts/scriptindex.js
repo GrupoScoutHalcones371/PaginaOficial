f897208e8d7093f3100bb9ced71e97443a0005fe
AnzaiaGitHub <66704419+AnzaiaGitHub@users.noreply.github.com>
2020-06-24 20:36:57 -0500

    Add files via upload

--- scripts/scriptindex.js
+++ scripts/scriptindex.js
@@ -3,55 +3,11 @@ var html = document.getElementsByTagName("html")[0],
 var teloner=false;
 const body = document.getElementById("body");
 
-const	modal1=document.getElementById('modal1');
-const	modal2=document.getElementById('modal2');
-
 const botonsubir = document.getElementById('scrolltop');
 
-function om(modalo){
-	switch(modalo){
-		case 1:
-			modal1.style.display='block';
-			modaler(0);		
-		break;
-		case 2:
-			modal2.style.display='block';
-			modaler(0);
-		break;
-	}
-}
-function cm(modalc) {
-	teloner=true;
-	switch(modalc){
-		case 1:
-			modal1.style.display='none';
-			modaler(1);
-		break;
-		case 2:
-			modal2.style.display='none';
-			modaler(1);
-		break;
-	}
-}
 
 function start(){
 	animate();
-}
-
-function modaler(opt){
-	if(opt==0){
-		body.style.overflow='hidden';
-		document.getElementById("telon").style.display='block';
-		teloner=true;
-		botonsubir.style.cursor='none';
-		botonsubir.style.opacity=0;
-	}else{
-		body.style.overflow='auto';
-		document.getElementById("telon").style.display='none';
-		teloner=false;
-		botonsubir.style.cursor='pointer';
-		botonsubir.style.opacity=1;
-	}
 }
 
 botonsubir.addEventListener('click',()=>{
